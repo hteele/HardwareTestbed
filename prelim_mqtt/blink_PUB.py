8addf615146647bb52097266163cbad7a76da9b4
Harrison Teele <hteele@stevens.edu>
2023-06-01 10:58:50 -0400

    Removed defunct MQTT lines

--- prelim_mqtt/blink_PUB.py
+++ prelim_mqtt/blink_PUB.py
@@ -5,28 +5,25 @@ from skyfield.api import load, wgs84, Topos, utc
 from dotenv import dotenv_values
 from config import PREFIX
 
+eph = load('de421.bsp')
+ts = load.timescale()
+
 def sunlit_status(satellite, observer, time):
     sunlit = satellite.at(time).is_sunlit(eph)
     return sunlit
 
-eph = load('de421.bsp')
-ts = load.timescale()
-
+# load SMCE credentials
 credentials = dotenv_values(".env")
 SMCE_HOST, SMCE_PORT = credentials["SMCE_HOST"], int(credentials["SMCE_PORT"])
 SMCE_USERNAME, SMCE_PASSWORD = credentials["SMCE_USERNAME"], credentials["SMCE_PASSWORD"]
-
-# MQTT_SERVER = "192.168.1.174"
-# MQTT_TPC1 = "hardware/blinkLED"
 TPC = f"{PREFIX}/hrdwr/blinkLED"
 
+# load selected satellite from Skyfield and print ID
 sat_url = 'http://celestrak.org/NORAD/elements/stations.txt'
 satellites = load.tle_file(sat_url)
-
 by_name = {sat.name: sat for sat in satellites}
 satellite = by_name['ISS (ZARYA)']
 print(satellite)
-
 
 # build the MQTT client
 CLIENT = mqtt.Client()
@@ -37,12 +34,6 @@ CLIENT.tls_set()
 # connect to MQTT server on port 8883
 CLIENT.connect(SMCE_HOST, SMCE_PORT)
 CLIENT.loop_start
-
-# client = mqtt.Client()
-# client.connect(MQTT_SERVER, 1883)
-# client.loop_start()
-
-observer = Topos(latitude_degrees = 40.744, longitude_degrees = 74.032)
 
 while True:
     now = datetime.utcnow()
@@ -55,8 +46,8 @@ while True:
         transit_status = "in shadow"
 
     print(timeS.astimezone(utc))
-
     print("Satellite is", transit_status)
+    
     CLIENT.publish(TPC, transit_status)
     print("Updating in 5 minutes...")
     time.sleep(300)
