b3fb4e4d26aeadc545d1dd3ceef6339adec5bc1a
Harrison Teele <hteele@stevens.edu>
2023-06-02 15:12:45 -0400

    Updated folder for wifi configurations + configured main.py

--- pico/main.py
+++ pico/main.py
@@ -2,31 +2,15 @@ import time
 import network
 from machine import Pin
 from umqtt.simple import MQTTClient
+from wificonfig import *
 
+standby = Pin("LED", Pin.OUT)
 led = Pin(16, Pin.OUT)
 
-wifi_ssid = "Stevens-IoT"
-wifi_password = "FSr474fTP3"
-
-wlan = network.WLAN(network.STA_IF)
-wlan.active(True)
-wlan.connect(wifi_ssid, wifi_password)
-while wlan.isconnected() == False:
-    print('Waiting for connection...')
-    time.sleep(1)
-print("Connected to WiFi")
-
-MQTT_SERVER = "192.168.1.174"
-#mqtt_username = "" 
-#mqtt_password = ""  
-MQTT_TPC = "satellite/sunlit/status" 
-MQTT_CLIENT = "Pico"
-
-mqtt_client = MQTTClient(client_id=MQTT_CLIENT, server=MQTT_SERVER)
-
-def on_message(topic, msg):
+def on_message(TPC, msg):
     message = msg.decode("utf-8")
-    print (f'Topic {topic} received message {message}')
+    topic = TPC.decode("utf-8")
+    print (f'Topic {topic} received message: {message}')
     if message == 'sunlit':
         print("LED ON")
         led.value(1)
@@ -34,16 +18,38 @@ def on_message(topic, msg):
         print("LED OFF")
         led.value(0)
 
-mqtt_client.set_callback(on_message)
-mqtt_client.connect()
-mqtt_client.subscribe(MQTT_TPC)
-print("Successfully subscribed to the follows topic(s): {MQTT_TPC}")
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True)
+wlan.connect(ssid, password)
+
+if wlan.isconnected == False:
+    sleep(1)
+
+print("Successfully Connected to " + ssid)
+TPC = f"{PREFIX}/hrdwr/blinkLED"
+PICOCLIENT = "Pico"
+
+CLIENT = MQTTClient(client_id=PICOCLIENT, server=SMCE_HOST, port=SMCE_PORT, user=SMCE_USERNAME, password=SMCE_PASSWORD, ssl=True)
+
+CLIENT.set_callback(on_message)
+CLIENT.connect()
+print("Connected to {SMCE_HOST}")
+CLIENT.subscribe(TPC)
+print("Successfully subscribed to the follows topic(s): {TPC}")
+# Onboard LED static if successfully connected
+standby.on()
 
 try:
     while True:
-        print(f'Waiting for messages on {MQTT_TPC}')
-        mqtt_client.wait_msg()
+        print(f'Waiting for messages on {TPC}')
+        CLIENT.wait_msg()
 except Exception as e:
     print(f'Failed to wait for MQTT messages: {e}')
+    # Flash onboard LED if connection failed
+    while True:
+        led.on()
+        time.sleep(1)
+        led.off()
+        time.sleep(1)
 finally:
-    mqtt_client.disconnect()
+    CLIENT.disconnect()
