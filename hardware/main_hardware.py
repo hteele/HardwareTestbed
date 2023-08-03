ef6b30dddf23779b6619adfbc45fea479b2a3414
hteele <123031133+hteele@users.noreply.github.com>
2023-08-03 12:35:00 -0400

    Update main_hardware.py

--- hardware/main_hardware.py
+++ hardware/main_hardware.py
@@ -1,42 +1,36 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Jul 10 15:25:21 2023
-
-@author: hteele
-"""
-
 import RPi.GPIO as GPIO
 import time
 from time import sleep
 import json
 from dotenv import dotenv_values
 import paho.mqtt.client as mqtt
+
 from config import (
     PREFIX,
     TPC
     )
 
-laser_pin = 11
+LED_pin = 11
 json_data = None
 
 def setup():
     GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(laser_pin, GPIO.OUT)
-    GPIO.output(laser_pin, GPIO.LOW)
+    GPIO.setup(LED_pin, GPIO.OUT)
+    GPIO.output(LED_pin, GPIO.LOW)
     
 def on_connect(CLIENT, userdata, flags, rc):
     CLIENT.subscribe(TPC)
         
 def on_message(CLIENT, userdata, msg):
     global json_data
+    global LED_pin
+    json
     comm_range = json_data.get("commRange")
     
     if comm_range:
-        GPIO.output(laser_pin, GPIO.HIGH)
-        print("Laser emitted")
+        GPIO.output(LED_pin, GPIO.HIGH)
     else:
-        GPIO.output(laser_pin, GPIO.LOW)
+        GPIO.output(LED_pin, GPIO.LOW)
     
 def mqtt_init():
     CLIENT = mqtt.Client()
@@ -48,19 +42,6 @@ def mqtt_init():
     print(f"Successfully connected to {HOST} on topic(s) {TPC}")
     CLIENT.loop_forever()
     
-    
-def loop():
-    while True:
-        GPIO.output(laser_pin, GPIO.LOW)
-        time.sleep(0.5)
-        GPIO.output(laser_pin, GPIO.HIGH)
-        time.sleep(0.5)
-        
-def destroy():
-    GPIO.OUTPUT(laser_pin, GPIO.HIGH)
-    GPIO.cleanup()
-    
-
 if __name__ == '__main__':
     credentials = dotenv_values(".env")
     HOST, PORT = credentials["HOST"], int(credentials["PORT"])
@@ -68,3 +49,4 @@ if __name__ == '__main__':
     setup()
     mqtt_init()
     
+GPIO.cleanup()
