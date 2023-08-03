8579623ffae8f7270de7d642da98e0f8b21ab203
hteele <123031133+hteele@users.noreply.github.com>
2023-08-03 17:23:36 -0400

    Update config.py

--- hardware/config.py
+++ hardware/config.py
@@ -5,6 +5,6 @@ NAME = os.getenv("NAME", "Hardware")
 LOG = f"\x1b[1m[\x1b[32m{NAME}\x1b[37m]\x1b[0m"
 HEADER = {
     "name": NAME,
-    "description": f'Emits laser to IR receiver on "{PREFIX}/constellation/location" topic.',
+    "description": f'Confirmation LED lights up upon message reception on  "{PREFIX}/constellation/detected" topic.',
 }
-TPC = f"{PREFIX}/constellation/location"
+TPC = f"{PREFIX}/constellation/detected"
